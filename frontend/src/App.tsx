0a894fdcfe5c65d5981dd0b4156480350bef3a7f
DemissonAndrade <demissondsa@gail.com>
2022-01-11 22:00:18 -0300

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
+import Form from "./pages/Form";
+import Listing from "./pages/Listing";
 
 
 function App() {
   return (
-    <NavBar />
+    <BrowserRouter>
+      <NavBar/>
+      <Routes>
+        <Route path="/" element={<Listing />} />
+        <Route path="/form">
+          <Route path=":movieId" element={<Form />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
