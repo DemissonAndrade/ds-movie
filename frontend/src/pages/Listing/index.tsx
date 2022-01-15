0d49c7487ce175d0be222d72d0699e10949c29a1
DemissonAndrade <demissondsa@gail.com>
2022-01-15 15:51:22 -0300

    first request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,17 +1,26 @@
 
 
+import axios from "axios";
+import { BASE_URL } from "util/requests";
 import MovieCard from "../../components/MovieCard";
 import Pagination from "../../components/Pagination";
 
 
 function Listing() {
 
+//FORMA ERRADA
+axios.get(`${BASE_URL}/movies?size=12&page=0`)
+.then(response => {
+    console.log(response.data);
+});
+
+
     return (
         <>
             <Pagination />
             <div className="container">
                 <div className="row">
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                    <div className=" col-sm-3 col-4 mb-3">
                         <MovieCard />
                     </div>
                     <div className="row">
